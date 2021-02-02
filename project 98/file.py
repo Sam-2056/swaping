8980920bf40260becab3e5f1a828c3e851890caa
Sam-2056 <74982421+Sam-2056@users.noreply.github.com>
2021-02-02 18:20:43 +0000

    Add files via upload

---
+++ project 98/file.py
@@ -0,0 +1,14 @@
+#im really sorry i forgot how to
+#swap the files so i made it to sawp some mubers
+
+x = 10
+y = 50
+  
+x = x + y    
+y = x - y   
+x = x - y  
+  
+print("Value of x:", x) 
+print("Value of y:", y) 
+
+    
